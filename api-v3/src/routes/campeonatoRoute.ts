34e561fcfa9705b9414fe2252149733d002d83e3
Guilherme Machado <136522300+guilhermesvm@users.noreply.github.com>
2024-11-07 11:31:47 -0300

    Delete api-v3 directory

--- api-v3/src/routes/campeonatoRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { postCampeonatos } from '../controllers/campeonatoController';
-
-const router = express.Router();
-
-router.post('/campeonatos', postCampeonatos);
-
-export default router;
--- api-v3/src/routes/partidaRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { postPartidas } from '../controllers/partidaController';
-
-const router = express.Router();
-
-router.post('/partidas', postPartidas);
-
-export default router;
